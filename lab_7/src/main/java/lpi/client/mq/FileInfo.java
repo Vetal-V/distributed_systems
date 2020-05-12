b16a35b2b0108a4aee6a58c1c1f18921e06899be
Vetal-V <54945817+Vetal-V@users.noreply.github.com>
2020-05-12 15:07:28 +0300

    Lab_7: Add mq client

---
+++ lab_7/src/main/java/lpi/client/mq/FileInfo.java
@@ -0,0 +1,13 @@
+package lpi.client.mq;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class FileInfo {
+    String sender;
+    String filename;
+    String content;
+}
